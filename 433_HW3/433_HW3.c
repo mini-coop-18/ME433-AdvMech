c95f29d614a38612ef0e5530346828093dc290bf
mini-coop-18 <gracecjackson@icloud.com>
2025-04-08 10:24:35 -0500

    USB Portion

--- 433_HW3/433_HW3.c
+++ 433_HW3/433_HW3.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-
+#include 
 
 int main()
 {
-    stdio_init_all();
-
+    stdio_init_all(); //enable the USB Communication 
+    while (!stdio_usb_connected()) { //waiting for the screen to open the port 
+        sleep_ms(100);
+    }
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
