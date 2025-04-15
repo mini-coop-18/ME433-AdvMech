50299d3ecb3041b99a495fb0a27144fa99aa3d65
mini-coop-18 <gracecjackson@icloud.com>
2025-04-15 13:04:16 -0500

    Beginnings of DAC Control

--- HW5/HW5.c
+++ HW5/HW5.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
+#include <math.h>
+
 
 // SPI Defines
 // We are going to use SPI 0, and allocate it to the following GPIO pins
@@ -12,6 +14,34 @@
 #define PIN_SCK  18
 #define PIN_MOSI 19
 
+#define WAVE_LEN 1000
+
+union FloatInt {
+    float f;
+    uint32_t i;
+};
+
+
+static volatile float Sin_Waveform[WAVE_LEN]; // waveforms
+
+
+void make_Sin_Waveform(){
+    for (int i=0; i<WAVE_LEN-1; i++){
+        Sin_Waveform[i] = 1.65*sin(i*4*3.14/(WAVE_LEN))+1.65; //done via a graphing calculator/trial and error
+    }
+}
+
+static inline void cs_select(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 0);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
+
+static inline void cs_deselect(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 1);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
 
 
 int main()
@@ -19,19 +49,22 @@ int main()
     stdio_init_all();
 
     // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000*1000);
+    spi_init(SPI_PORT, WAVE_LEN*);
     gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
     gpio_set_function(PIN_CS_DAC,   GPIO_FUNC_SIO);
+    gpio_set_function(PIN_CS_RAM,   GPIO_FUNC_SIO);
     gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
     gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
     
     // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_set_dir(PIN_CS, GPIO_OUT);
+    gpio_set_dir(PIN_CS_DAC, GPIO_OUT);
     gpio_put(PIN_CS, 1);
+    gpio_set_dir(PIN_CS_RAM, GPIO_OUT);
+    gpio_put(PIN_CS, 1);   
     // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        static int c = 0;
+
     }
 }
