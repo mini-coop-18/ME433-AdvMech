ec0a45db65f97b6a82bd92f7b1a17efce59bb7aa
mini-coop-18 <gracecjackson@icloud.com>
2025-05-29 12:55:02 -0500

    Just hanging out

--- WheelControl/WheelControl.c
+++ WheelControl/WheelControl.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
 
+#define LEFT_WHEEL 17
+#define LEFT_BLINKER 15
 
 int main()
 {
     stdio_init_all();
+    gpio_init(LEFT_WHEEL);  
+    gpio_set_dir(LEFT_WHEEL, 1); 
+    gpio_put(LEFT_WHEEL, 1);
 
+    gpio_init(LEFT_BLINKER);  
+    gpio_set_dir(LEFT_BLINKER, 1); 
+    gpio_put(LEFT_BLINKER, 1);
     while (true) {
     
     }
